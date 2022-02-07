bd3728676e929381aaf30381256ba3972d4d417b
suri <sp48840@gmail.com>
2022-02-07 20:27:01 +0530

    initial commit

---
+++ suri_dev/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (<>
+  <div className="nav">
+    <ul className='nav_list'>
+      <li className='nav_item'><a href='#'>home</a></li>
+      <li className='nav_item'><a href='#'>blog</a></li>
+      <li className='nav_item'><a href='#'>contact</a></li>
+      <li className='nav_item'><a href='#'> about us</a></li>
+    </ul>
+  </div>
+  
+  <div className="blank"> Suraj bhai ki react site</div>
+  </>
+ 
+  );
+}
+
+export default App;
